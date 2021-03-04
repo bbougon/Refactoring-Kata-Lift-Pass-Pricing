38b82372150a4e695834e0400386127a996a64cb
Bertrand Bougon <bertrandbougon@gmail.com>
2021-03-04 19:54:49 +0100

    Continuation du refacto, extraction de 2 repositories (Holidays et BaseCost), et déplacement du calcul de la réduction dans la classe Price

--- java/src/main/java/dojo/liftpasspricing/Prices.java
+++ java/src/main/java/dojo/liftpasspricing/Prices.java
@@ -33,7 +33,8 @@ public class Prices {
             return cost;
         }
 
-        private static int computePriceThatIsNotDuringNight(final Integer age, final int reduction, final int baseCost) {
+        private static int computePriceThatIsNotDuringNight(final Integer age, final int baseCost, Date date, boolean isHoliday) {
+            int reduction = computeDiscount(date, isHoliday);
             int cost;
             if (age != null && age < 15) {
                 cost = (int) Math.ceil(baseCost * .7);
@@ -49,6 +50,18 @@ public class Prices {
                 }
             }
             return cost;
+        }
+
+        private static int computeDiscount(final Date date, final boolean isHoliday) {
+            int reduction= 0;
+            if (date != null) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+                if (!isHoliday && calendar.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY) {
+                    reduction = 35;
+                }
+            }
+            return reduction;
         }
     }
 
@@ -78,60 +91,25 @@ public class Prices {
             final Integer age = req.queryParams("age") != null ? Integer.valueOf(req.queryParams("age")) : null;
 
             int cost = 0;
-            try (PreparedStatement costStmt = connection.prepareStatement( //
-                    "SELECT cost FROM base_price " + //
-                            "WHERE type = ?")) {
-                costStmt.setString(1, req.queryParams("type"));
-                try (ResultSet result = costStmt.executeQuery()) {
-                    result.next();
-                    int baseCost = result.getInt("cost");
+            String type = req.queryParams("type");
 
-                    int reduction;
-                    boolean isHoliday;
+            int baseCost = BaseCostRepository.getCostByType(type, connection);
 
-                    if (age != null && age < 6) {
-                        cost = 0;
-                    } else {
-                        if (!req.queryParams("type").equals("night")) {
-                            DateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd");
+            if (age == null || age >= 6) {
+                if (!type.equals("night")) {
+                    String stringDate = req.queryParams("date");
 
-                            String stringDate = req.queryParams("date");
-                            Date date = null;
-                            if (stringDate != null) {
-                                date = isoFormat.parse(stringDate);
-                            }
-                            try (PreparedStatement holidayStmt = connection.prepareStatement( //
-                                    "SELECT * FROM holidays")) {
-                                Holidays holidays2 = new Holidays();
-
-                                try (ResultSet holidays = holidayStmt.executeQuery()) {
-
-                                    while (holidays.next()) {
-                                        holidays2.add(holidays.getDate("holiday"));
-
-                                    }
-
-                                }
-                                final Date date2 = date;
-                                isHoliday = holidays2.holidays().stream().anyMatch(holiday ->
-                                {
-                                    if (date2 != null) {
-                                        return date2.getYear() == holiday.getYear() && //
-                                                date2.getMonth() == holiday.getMonth() && //
-                                                date2.getDate() == holiday.getDate();
-                                    }
-                                    return false;
-                                });
-                            }
-
-                            reduction = computeDiscount(date, isHoliday);
-
-                            // TODO apply reduction for others
-                            cost = Price.computePriceThatIsNotDuringNight(age, reduction, baseCost);
-                        } else {
-                            cost = Price.computePriceDuringNight(age, baseCost);
-                        }
+                    Date date = null;
+                    if (stringDate != null) {
+                        DateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd");
+                        date = isoFormat.parse(stringDate);
                     }
+                    boolean isHoliday = HolidaysRepository.isDuringHoliday(date, connection);
+
+                    // TODO apply reduction for others
+                    cost = Price.computePriceThatIsNotDuringNight(age, baseCost, date, isHoliday);
+                } else {
+                    cost = Price.computePriceDuringNight(age, baseCost);
                 }
             }
 
@@ -145,16 +123,49 @@ public class Prices {
         return connection;
     }
 
-    private static int computeDiscount(final Date date, final boolean isHoliday) {
-        int reduction = 0;
-        if (date != null) {
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(date);
-            if (!isHoliday && calendar.get(Calendar.DAY_OF_WEEK) == 2) {
-                reduction = 35;
+    private static class BaseCostRepository{
+
+        static int getCostByType(final String type, Connection connection) throws SQLException {
+            try (PreparedStatement costStmt = connection.prepareStatement( //
+                    "SELECT cost FROM base_price " + //
+                            "WHERE type = ?")) {
+                costStmt.setString(1, type);
+                try (ResultSet result = costStmt.executeQuery()) {
+                    result.next();
+                    return result.getInt("cost");
+                }
             }
         }
-        return reduction;
+    }
+
+    private static class HolidaysRepository {
+
+        public static boolean isDuringHoliday(final Date date, Connection connection) throws SQLException {
+            boolean isHoliday;
+            try (PreparedStatement holidayStmt = connection.prepareStatement( //
+                    "SELECT * FROM holidays")) {
+                Holidays holidays = new Holidays();
+
+                try (ResultSet holidaysResultSet = holidayStmt.executeQuery()) {
+
+                    while (holidaysResultSet.next()) {
+                        holidays.add(holidaysResultSet.getDate("holiday"));
+                    }
+
+                }
+                isHoliday = holidays.holidays().stream().anyMatch(holiday ->
+                {
+                    if (date != null) {
+                        return date.getYear() == holiday.getYear() && //
+                                date.getMonth() == holiday.getMonth() && //
+                                date.getDate() == holiday.getDate();
+                    }
+                    return false;
+                });
+            }
+            return isHoliday;
+        }
+
     }
 
 
